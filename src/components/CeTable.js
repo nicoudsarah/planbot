3bc9aaa8de7e940d032f49ea47220f7319093c3d
nicoudsarah <sarah@sogilis.com>
2021-03-03 14:34:38 +0100

    [dev] insertion of JSON files datas in the table

--- src/components/CeTable.js
+++ src/components/CeTable.js
@@ -1,28 +1,75 @@
-import React from "react";
-import PropTypes from 'prop-types';
+import React, {useEffect, useState} from "react";
+import Json_CE_All_YEAR_2021_PROJ_Dev from "../CE_All_YEAR_2021_PROJ_Dev.json";
+import Json_CE_Grenoble_YEAR_2021_PROJ_Dev from "../CE_Grenoble_YEAR_2021_PROJ_Dev.json"
+import Json_CE_Lyon_YEAR_2021_PROJ_Dev from "../CE_Lyon_YEAR_2021_PROJ_Dev.json"
+import Json_CE_Lyon_YEAR_2021_PROJ_All from "../CE_Lyon_YEAR_2021_PROJ_All.json"
+import Json_CE_Lyon_YEAR_2021_PROJ_Ergo from "../CE_Lyon_YEAR_2021_PROJ_Ergo.json"
+import Json_CE_Lyon_YEAR_2020_PROJ_Dev from "../CE_Lyon_YEAR_2020_PROJ_Dev.json"
 
-const CeTable = (props) => {
+const CeTable = ({ce, projectType, year}) => {
+
+    const indicators =  ["CA", "TJM", "# Jours dispo",  "# Jours prod", "# Jours interP", "TO"]
+
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        /* Pour Ruby
+        const JSONRecup = async () => {
+            const response = await fetch(data,{
+                headers : {
+                    'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                }
+            })
+            console.log(response)
+        }
+        JSONRecup()*/
+        if( ce === "all" && year === "2021" && projectType === "dev" ) {
+            setData(Json_CE_All_YEAR_2021_PROJ_Dev)
+        } else if (ce === "grenoble" && year === "2021" && projectType === "dev" ) {
+            setData(Json_CE_Grenoble_YEAR_2021_PROJ_Dev)
+        } else if (ce === "lyon" && year === "2021" && projectType === "dev" ) {
+            setData(Json_CE_Lyon_YEAR_2021_PROJ_Dev)
+        } else if (ce === "lyon" && year === "2021" && projectType === "all" ) {
+            setData(Json_CE_Lyon_YEAR_2021_PROJ_All)
+        } else if (ce === "lyon" && year === "2021" && projectType === "ergo" ) {
+            setData(Json_CE_Lyon_YEAR_2021_PROJ_Ergo)
+        } else if (ce === "lyon" && year === "2020" && projectType === "dev" ) {
+            setData(Json_CE_Lyon_YEAR_2020_PROJ_Dev)
+        }
+
+    }, [ce, projectType, year])
+    const months = data && Object.keys(data)
+    console.log(months)
+
+
     return (
         <>
             <table>
                 <thead>
                     <tr>
-                        <td></td>
-                        <td colSpan="6" id="monthly-values">Valeurs mensuelles</td>
-                        <td colSpan="6" id="annual-values">Valeurs annuelles à date</td>
+                        <th></th>
+                        <th colSpan="6" >Valeurs mensuelles</th>
+                        <th colSpan="6" >Valeurs annuelles à date</th>
                     </tr>
                     <tr>
-                        <td id="month-cell">Mois</td>
-                        {props.gestionDatas && props.gestionDatas.map((item, index) =>
-                            <td key={index} id={"monthly-" + item.name}>{item.indicator}</td>)}
-                        {props.gestionDatas && props.gestionDatas.map((item, index) =>
-                            <td key={index} id={"annual-" + item.name}>{item.indicator}</td>)}
+                        <th id="month-cell">Mois</th>
+                        {indicators.map((item, index) =>
+                            <th key={index} >{item}</th>)}
+                        {indicators.map((item, index) =>
+                            <th key={index} >{item}</th>)}
                     </tr>
                 </thead>
                 <tbody>
-                    {props.calender && props.calender.map((item, index) =>
-                        <tr key={index} id={item.id}>
-                            <td key={index} id={item.id + "-cell"}>{item.month}</td>
+                    {data && months && months.map((month, index) =>
+                        <tr key={index}>
+                            <td key={index}>{month}</td>
+                            <td>{data[month].CA}</td>
+                            <td>{data[month].TJM}</td>
+                            <td>{data[month].availableDays}</td>
+                            <td>{data[month].productionDays}</td>
+                            <td>{data[month].interProductionDays}</td>
+                            <td>{data[month].TO}</td>
                         </tr>)}
                 </tbody>
             </table>
@@ -30,10 +77,5 @@ const CeTable = (props) => {
     )
 }
 
-CeTable.propTypes = {
-    gestionDatas: PropTypes.array.isRequired,
-    calender: PropTypes.array.isRequired
-    // est-ce que j'ai besoin de spécifier que l'id et le month de calender sont obligatoire ? Pareil pour le name et l'indicator de gestionDatas ?
-}
 
 export default CeTable
