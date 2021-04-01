1d1400ebd6b3b3844f7470b09aa0534376d0d421
nicoudsarah <sarah@sogilis.com>
2021-04-01 16:25:36 +0200

    [dev] insert function with test for calculate cumulate datas for available days, production days and interproduction days metrics (function in progress)

---
+++ src/DataProcessing.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const computeSum = (values) => {
+    return values.reduce((a, b) => a + b, 0);
+}
+
+export class DataProcessing extends React.Component {
+
+    static calculateValueToDisplayForProductionMetrics(months, productionMetricsValues, productionMetricsLabel) {
+        let eachMonthProductionMetricValues = null
+        let collectionOfProductionMetricsValuesAdded = []
+        months.forEach((month, index) => {
+            eachMonthProductionMetricValues = productionMetricsValues.slice(0, index + 1); // On récupère les valeurs de la métrique de chaque mois
+            const sumOfProductionMetricValues = computeSum(eachMonthProductionMetricValues);  //on somme ces valeurs
+            collectionOfProductionMetricsValuesAdded.push(sumOfProductionMetricValues);
+        })
+        return collectionOfProductionMetricsValuesAdded
+    }
+}
+
+
+
+
+
